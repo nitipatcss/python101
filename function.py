9eb4d68e16b69798d21f4b4cf6f0b0890bf32f00
nitipatcss <nitipatcs@gmail.com>
2019-02-06 15:18:40 +0700

    edit temperature file agian

--- function.py
+++ function.py
@@ -10,8 +10,6 @@
 #     print("Prcie without VAT will be " + str(exc_vat(price)))
 
 import sys
-
-
 def check_temp(temp):
     if temp < 15:
         return ("It's so cool")
@@ -19,18 +17,21 @@ def check_temp(temp):
         return ("It good weather")
     elif temp < 30:
         return ("It warm")
-    elif temp < 35:
+    elif temp < 40:
         return ("It so hot")
     else:
         return ("shit, It fucking hot")
 
-
 if __name__ == '__main__':
     temp = input("enter your temperature: ")
     while temp != 'q':
+
         if temp.isnumeric():
             temp = int(temp)
             print(check_temp(temp))
         else:
-            temp = int(input("enter your temp again.. :"))
+            print("You must enter mumber or 'q' only.")
+
+        temp = int(input("enter your temp again.. :"))
+
     sys.exit(0)
